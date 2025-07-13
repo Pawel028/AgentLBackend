92a0439264ae35e2566292dcf8785d27f3d748da
Pawel028 <pawelsogra10@gmail.com>
2025-07-13 08:06:06 +0530

    Adding try-except for logging errors

--- app.py
+++ app.py
@@ -138,5 +138,5 @@ def upload_file():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))  # Azure uses this env var
-    app.run(host='0.0.0.0', port=port)   
+    app.run(host='0.0.0.0', port=port,debug=True)   
 
